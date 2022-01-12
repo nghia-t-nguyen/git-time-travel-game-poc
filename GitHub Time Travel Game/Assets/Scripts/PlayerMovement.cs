3f7eaf34acbfe55ea2f834faec92d8c167aec5a8
nghia-t-nguyen <nguyenn2001@gmail.com>
2022-01-12 02:19:20 -0500

    initial commit
    Idle and Walk Animation, Camera Follow, and Point/Click Movement

---
+++ GitHub Time Travel Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private Rigidbody2D body;
+    [SerializeField] private float speed;
+    private float scaling_factor;
+    private Animator anim;
+    private Vector2 target;
+    private float horizontalInput;
+
+    private void Awake() {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        scaling_factor = 2;
+        transform.localScale = Vector3.one*scaling_factor;
+    }
+
+    private void Update() {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0)) {
+            if (System.Math.Abs(body.position.x-mousePos.x) > 1.5f)
+                target = new Vector2(mousePos.x, 0);
+            else
+                target = transform.position;
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime * 5f);
+
+        horizontalInput = target.x - transform.position.x;
+
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one*scaling_factor;
+        else if (horizontalInput < -0.01f)
+            transform.localScale =  new Vector3(-1,1,1)*scaling_factor;
+
+        anim.SetBool("run", horizontalInput != 0);
+    }
+}
